4b739850d005410b0428c97de0163e07797cd33a
Marc Hernández Navarro <marcmhn@hotmail.com>
2016-03-09 10:31:17 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var mlist = {
+    "arrow-circle-left": "Atrás",
+    "undo": "Deshacer",
+    "refresh": "Recargar",
+    "question-circle": "Ayuda",
+    "close": "Cerrar"
+};
+
+var mlinks = Array(
+    "alert('Seleccionada la opción atrás')",
+    "alert('Seleccionada la opción Deshacer')",
+    "alert('Seleccionada la opción Recargar')",
+    "alert('Seleccionada la opción Ayuda')",
+    "alert('Seleccionada la opción Cerrar')"
+);
+
+var cm = new ContextualMenu(mlist, mlinks);
